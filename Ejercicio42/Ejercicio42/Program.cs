645da63652641f2d396f0508599f7aa2aeaa3c08
Enrique Green <47195508+BruceManuelwayne@users.noreply.github.com>
2021-05-02 15:35:37 -0300

    reverse array

---
+++ Ejercicio42/Ejercicio42/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ingreso;
+            bool ingresoCorrecto = false;
+            Console.WriteLine("Ingrese una palabra para ser revertida:");
+            //faltaria una validacion de solo letras:¿
+
+            do
+            {
+                ingreso = Console.ReadLine();
+                if (string.IsNullOrEmpty(ingreso))
+                {
+                    Console.WriteLine("ingrese una palabra valida: ");
+                    continue;
+
+                }
+                else
+                {
+                    char[] arr = ingreso.ToCharArray();
+                    Array.Reverse(arr);
+                    string reverse = new string(arr); 
+                    Console.WriteLine(reverse);
+                    break;
+
+
+                }
+            } while (!ingresoCorrecto); 
+            Console.ReadKey();
+            
+        }
+    }
+}
